438b1f90efdb7208eef92e8eacac4db8d43206c4
Westin Wrzesinski <westinw@slalom.com>
2015-10-21 16:05:05 -0500

    Better Header Tests

--- app/components/Header/Header-test.jsx
+++ app/components/Header/Header-test.jsx
@@ -4,6 +4,15 @@ import TestUtils from 'react-addons-test-utils';
 import { expect } from 'chai';
 import Header from './Header';
 
+Header.__Rewire__('page', () => {});
+
+let searchAction = (component, query) => {
+  let search = component.querySelector('.search-input'),
+      form = component.querySelector('form');
+      search.value='Query';
+  TestUtils.Simulate.change(search);
+  TestUtils.Simulate.submit(form);
+};
 
 describe('Components', () => {
   describe('Header', () => {
@@ -20,16 +29,18 @@ describe('Components', () => {
       expect(result).to.be.defined;
     });
 
-    xit('should update the search query when user enteres one', () => {
-      //TODO: Test + Stub Page.js
+    it('should update the search query when user enteres one', () => {
+      let result = TestUtils.findRenderedDOMComponentWithClass(component, 'app-header');
+      searchAction(result, 'Query');
+      expect(result.querySelector('.term').textContent).to.equal('Query');
     });
 
-    xit('should fire a sort action on user sort', () => {
-      //TODO: Test + Spy AppActions
-    });
-
-    xit('should reset a search back to input box', () => {
-      //TODO: Test + Stub Page.js
+    it('should reset a search back to input box', () => {
+      let result = TestUtils.findRenderedDOMComponentWithClass(component, 'app-header');
+      searchAction(result, 'Query');
+      expect(result.querySelector('form')).to.be.null;
+      TestUtils.Simulate.click(result.querySelector('.fa-times'));
+      expect(result.querySelector('form')).to.be.defined;
     });
   });
 });
