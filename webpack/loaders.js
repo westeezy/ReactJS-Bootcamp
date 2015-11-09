68d687662097e5ea62fd69f08f6596726e5ca71d
Westin Wrzesinski <westinw@slalom.com>
2015-11-09 10:56:52 -0600

    Cleanup package.json and loaders

--- webpack/loaders.js
+++ webpack/loaders.js
@@ -75,15 +75,12 @@ var loaders = [
     test: /\.css$/,
     loader: cssLoader
   },
-  { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-    loader: 'url-loader?limit=10000&minetype=application/font-woff'
+  {
+    test: /\.(ttf|eot|svg|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+    loader: fileLoader
   },
   {
-    test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-    loader: 'file-loader'
-  },
-  {
-    test: /\.jpe?g$|\.gif$|\.png$|\.ico|\.svg$|\.woff$|\.woff2$|\.eot$|\.ttf$/,
+    test: /\.jpe?g$|\.gif$|\.png$/,
     loader: fileLoader
   },
   {
